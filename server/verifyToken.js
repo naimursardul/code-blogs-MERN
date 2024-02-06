d658709c48d72dfae4159e39f4b949dd75417809
Naimur <naimur30447@gmail.com>
2024-02-06 23:08:58 +0600

    cmt

--- server/verifyToken.js
+++ server/verifyToken.js
@@ -5,10 +5,11 @@ dotenv.config();
 
 const verifyToken = async (req, res, next) => {
   const token = req.cookies.token;
-  // console.log("token " + token);
+  console.log("token " + token);
   if (token) {
     jwt.verify(token, process.env.SECRET, async (err) => {
       if (err) {
+        console.log(err);
         res.status(403).json({ message: "Token is not valid!" });
       } else {
         next();
